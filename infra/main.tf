6a0e1c587e67a27834f2b61a837880d5b9153f94
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2025-03-22 03:51:26 -0300

    Load balancer configuration

--- infra/main.tf
+++ infra/main.tf
@@ -31,7 +31,7 @@ resource "aws_key_pair" "chaveSSH"{
 
 
 resource "aws_autoscaling_group" "grupo" {
-  availability_zones = [ "${var.regiao_aws}a" ]
+  availability_zones = [ "${var.regiao_aws}a", "${var.regiao_aws}b" ]
   name = var.nomeGrupo
   max_size = var.maximo
   min_size = var.minimo
@@ -39,4 +39,39 @@ resource "aws_autoscaling_group" "grupo" {
     id = aws_launch_template.maquina.id
     version = "$Latest"
   }
+  target_group_arns = [ aws_lb_target_group.alvoLoadBalancer.arn ]
+}
+
+resource "aws_default_subnet" "subnet_1"{
+  availability_zones =  "${var.regiao_aws}a"
+}
+
+resource "aws_default_subnet" "subnet_2"{
+  availability_zones =  "${var.regiao_aws}b"
+}
+
+resource "aws_lb" "loadBalancer"{
+  internal = false
+  subnets = [ aws_default_subnet.subnet_1.id,aws_default_subnet.subnet_2.id ]
+}
+
+resource "aws_lb_target_group" "alvoLoadBalancer"{
+  name = "maquinasAlvo"
+  port = "8000"
+  protocol = "HTTP"
+  vpc_id = aws_default_vpc.default.id
+}
+
+resource "aws_lb_listener" "entradaLoadBalancer"{
+  load_balancer_arn = aws_lb.loadBalancer.arn
+  port = "8000"
+  protocol = "HTTP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.alvoLoadBalancer.arn
+  }
+}
+
+resouce "aws_default_vpc" "default"{
+
 }
